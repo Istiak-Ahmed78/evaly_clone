04a52a5c7ab36a901b86d3636527d116e876d9b4
Istiak Ahmed <aistiak2078@gmail.com>
2021-06-18 15:55:44 +0600

    push notification_done

---
+++ lib/utils/firebase/firebase_methodes.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseMethodes {
+  late FirebaseAuth authReference;
+  FirebaseMethodes() {
+    authReference = FirebaseAuth.instance;
+  }
+  Future<UserCredential> logInWithEmailAndPassword(
+          {required email, required password}) =>
+      authReference.signInWithEmailAndPassword(
+          email: email, password: password);
+  Future<UserCredential> signUpWithEmailAndPassword(
+          {required email, required password}) =>
+      authReference.createUserWithEmailAndPassword(
+          email: email, password: password);
+  logOut() => authReference.signOut();
+}
--- lib/views/screens/nav_items/account_tab/account_tab.dart
+++ lib/views/screens/nav_items/account_tab/account_tab.dart
@@ -1,5 +1,4 @@
 import 'package:evaly_clone/state_management/account_tab_state.dart';
-import 'package:evaly_clone/views/screens/nav_items/account_tab/logged_in/logged_in_page.dart';
 import 'package:evaly_clone/views/screens/nav_items/account_tab/tabs/log_in/log_in.dart';
 import 'package:evaly_clone/views/screens/nav_items/account_tab/tabs/sign_up/sign_up.dart';
 import 'package:flutter/material.dart';
